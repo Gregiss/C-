7cf01b31c4c4b5d41d63e04c5baf6a32d2b5f48f
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-08-13 20:59:34 -0300

    Initial project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldo = 0;
+            int operation = 0;
+            bool error = false;
+            String hash;
+            Console.WriteLine("Olá ao iBank\n Seu saldo atual é de "+saldo);
+            while (true)
+            {
+                Console.WriteLine("\n Escreva as operações que queres\n 1-Depositar\n 2-Retirar saldo");
+                operation = Convert.ToInt32(Console.ReadLine());
+                if (operation == 1)
+                {
+                    Console.WriteLine("Qual o valor que queres depositar em sua conta?");
+                    int newSaldo = Convert.ToInt32(Console.ReadLine());
+                    saldo += newSaldo;
+                    Console.WriteLine("Seu saldo atual é de " + saldo);
+                    operation = 0;
+                } else if(operation == 2)
+                {
+                    Console.WriteLine("Qual o valor que queres retirar em sua conta?");
+                    error = true;
+                    while (error)
+                    {
+                        int newSaldo = Convert.ToInt32(Console.ReadLine());
+                        if (saldo < newSaldo)
+                        {
+                            Console.WriteLine("Não podes retirar um valor que não tenhas");
+                        }
+                        else
+                        {
+                            saldo -= newSaldo;
+                            error = false;
+                            Console.WriteLine("Seu saldo atual é de " + saldo);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ JogoDaVelha/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vez = -1;
+            bool comecou = true;
+            int[,] matriz = new int[3 , 3];
+
+            //Prenche a matriz
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz[i, j] = -1;
+                }
+            }
+
+                    String quem = "0";
+            //Cria a matriz
+            for(int i = 0; i < 9; i++)
+            {
+                if( i == 2)
+                {
+                    Console.Write(" - \n");
+                } else if (i == 5)
+                {
+                    Console.Write(" - \n");
+                }
+                else
+                {
+                    Console.Write(" - ");
+                }
+            }
+            while (comecou)
+            {
+                Console.WriteLine("\nEscreva qual posicao X queres jogar");
+                int novaJogadaX = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escreva qual posicao Y queres jogar");
+                int novaJogadaY = Convert.ToInt32(Console.ReadLine());
+                if (vez == 0)
+                {
+                    vez = 1;
+                }
+                else
+                {
+                    vez = 0;
+                }
+                matriz[novaJogadaX, novaJogadaY] = vez;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (matriz[i, j] == 0)
+                        {
+                            quem = "O";
+                        }
+                        else if (matriz[i, j] == 1)
+                        {
+                            quem = "X";
+                        }
+                        else
+                        {
+                            quem = "  -  ";
+                        }
+                        if (j == 2)
+                        {
+                            Console.Write("            " + quem + "     " + "\n");
+                        }
+                        else if (i == 3)
+                        {
+                            Console.Write("            " + quem + "     " + "\n");
+                        }
+                        else
+                        {
+                            Console.Write("            " + quem + "     ");
+                        }
+                    }
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (matriz[0, 0] == 0 && matriz[0, 1] == 0 && matriz[0, 2] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[1, 0] == 0 && matriz[1, 1] == 0 && matriz[1, 2] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[2, 0] == 0 && matriz[2, 1] == 0 && matriz[2, 2] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 0] == 0 && matriz[1, 0] == 0 && matriz[2, 0] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 1] == 0 && matriz[1, 1] == 0 && matriz[2, 1] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 2] == 0 && matriz[1, 2] == 0 && matriz[2, 2] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 1] == 0 && matriz[1, 1] == 0 && matriz[2, 2] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 2] == 0 && matriz[0, 1] == 0 && matriz[2, 0] == 0)
+                        {
+                            Console.WriteLine("O ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        //verifica x
+                        if (matriz[0, 0] == 1 && matriz[0, 1] == 1 && matriz[0, 2] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[1, 0] == 1 && matriz[1, 1] == 1 && matriz[1, 2] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[2, 0] == 1 && matriz[2, 1] == 1 && matriz[2, 2] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 0] == 1 && matriz[1, 0] == 1 && matriz[2, 0] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 1] == 1 && matriz[1, 1] == 1 && matriz[2, 1] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 2] == 1 && matriz[1, 2] == 1 && matriz[2, 2] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 1] == 1 && matriz[1, 1] == 1 && matriz[2, 2] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                        else if (matriz[0, 2] == 1 && matriz[0, 1] == 1 && matriz[2, 0] == 1)
+                        {
+                            Console.WriteLine("X ganhou");
+                            comecou = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
