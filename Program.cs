e1e13a565a03be19e0f60563af7c2dae3190d8ae
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-08-13 21:51:54 -0300

    Hipotenusa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hipotenusa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = 3, b = 4, a = 5;
+            b = Math.Pow(b, 2);
+            c = Math.Pow(c, 2);
+            double hipotenusa = b + c;
+            hipotenusa = Math.Sqrt(hipotenusa);
+            Console.WriteLine(hipotenusa);
+            Console.ReadLine();
+        }
+    }
+}
