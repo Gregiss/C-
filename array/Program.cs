11d7b8c21b5558f09ef1a6fd69724b053ab33c9f
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-08-20 21:52:14 -0300

    Ok

---
+++ array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] arrayNomes = new String[10];
+            for(int i = 0; i < arrayNomes.Length; i++)
+            {
+                Console.Write($"Informe o nome? {i + 1} : ");
+                arrayNomes[i] = Console.ReadLine();
+                Console.WriteLine("");
+            }
+            Array.Sort(arrayNomes, StringComparer.InvariantCultureIgnoreCase);
+            Array.ForEach(arrayNomes, x => Console.WriteLine(x));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ media/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] notas = new String[10, 2];
+            int media = 0, nomeIndice = 0;
+            String aux = "";
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Qual o nome do aluno {i + 1} :");
+                notas[i, nomeIndice] = Console.ReadLine();
+                aux = notas[i, 0];
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Qual a {j + 1} nota de {aux} : ");
+                    notas[i, j] = Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < notas.Length; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
