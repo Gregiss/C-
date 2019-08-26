7d1994cde61648f3b6bc6527466709c6f354a733
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-08-26 19:18:16 -0300

    Fix

--- media/Program.cs
+++ media/Program.cs
@@ -1,34 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace media
+namespace MediaAlunos
 {
     class Program
     {
         static void Main(string[] args)
         {
             String[,] notas = new String[10, 2];
-            int media = 0, nomeIndice = 0;
-            String aux = "";
+            double[] mediaAluno = new double[10];
+            String[] auxNome = new String[10];
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine($"Qual o nome do aluno {i + 1} :");
-                notas[i, nomeIndice] = Console.ReadLine();
-                aux = notas[i, 0];
+                notas[i, 0] = Console.ReadLine();
+                auxNome[i] = notas[i, 0];
                 for (int j = 0; j < 2; j++)
                 {
-                    Console.WriteLine($"Qual a {j + 1} nota de {aux} : ");
+                    Console.WriteLine($"Qual a {j + 1} nota de {auxNome[i]} : ");
                     notas[i, j] = Console.ReadLine();
                 }
             }
-            for (int i = 0; i < notas.Length; i++)
+            for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-
+                    mediaAluno[i] = ( Convert.ToDouble(notas[i, j]) + Convert.ToDouble(notas[i, j]) ) / 2;
+                    if(j >= 1)
+                    {
+                        Console.WriteLine($"{auxNome[i]} media {mediaAluno[i] }");
+                    }
                 }
             }
             Console.ReadLine();
